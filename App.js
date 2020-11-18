d1d364dd7538bbc0ddde819ff9b5816e00773518
amrtawfik160 <amrtawfik160@gmail.com>
2020-11-18 21:56:23 +0200

    setup the environment and the weather api

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
+const WEATHER_API_KEY = '02026f5ad728cd8e47d74c0f9518d4f9';
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [currentWeather, setCurrentWeather] = useState(null);
+    const [unitSystem, setUnitSystem] = useState('metric');
+
+    useEffect(() => {
+        load();
+    }, []);
+
+    const load = async () => {
+        try {
+            // const { status } = await Location.requestPermissionsAsync();
+            // if (status !== 'granted') {
+            //     setErrorMessage('Access to location is needed to run the app');
+            //     return;
+            // }
+            // const location = await Location.getCurrentPositionAsync();
+            // const { latitude, longitude } = location.coords;
+            // const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`;
+            const weatherUrl = `${BASE_WEATHER_URL}lat=-37.883642&lon=145.181741&units=${unitSystem}&appid=${WEATHER_API_KEY}`;
+
+            const res = await fetch(weatherUrl);
+            const result = await res.json();
+
+            if (res.ok) {
+                setCurrentWeather(result);
+            } else {
+                setErrorMessage(result.message);
+            }
+        } catch (error) {
+            setErrorMessage(error.message);
+        }
+    };
+
+    if (currentWeather) {
+        const {
+            main: { temp },
+        } = currentWeather;
+
+        return (
+            <View style={styles.screen}>
+                <StatusBar style='auto' />
+                <View style={styles.main}>
+                    <Text>{temp}</Text>
+                </View>
+            </View>
+        );
+    } else {
+        // Return if there is a error
+        return (
+            <View style={styles.screen}>
+                <StatusBar style='auto' />
+                <Text>{errorMessage}</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    main: {
+        justifyContent: 'center',
+        flex: 1,
+    },
 });
