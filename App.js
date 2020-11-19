74a1a27066e9ee2ffac7cb3e48b65b0be43671b1
amrtawfik160 <amrtawfik160@gmail.com>
2020-11-19 10:07:39 +0200

    add Weather info component

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
+import Constants from 'expo-constants';
+
+import WeatherInfo from './components/WeatherInfo';
 
 const WEATHER_API_KEY = '02026f5ad728cd8e47d74c0f9518d4f9';
 const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
@@ -12,44 +15,40 @@ export default function App() {
     const [unitSystem, setUnitSystem] = useState('metric');
 
     useEffect(() => {
-        load();
+        (async () => {
+            try {
+                /** ===== This url for test in Android device ===== **/
+                const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=30.0778&lon=31.2852&units=metric&appid=02026f5ad728cd8e47d74c0f9518d4f9`;
+
+                // let { status } = await Location.requestPermissionsAsync();
+                // if (status !== 'granted') {
+                //     setErrorMessage('Access to location is needed to run the app');
+                //     return;
+                // }
+                // let location = await Location.getCurrentPositionAsync();
+                // const { latitude, longitude } = location.coords;
+                // const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`;
+
+                const res = await fetch(weatherUrl);
+                const result = await res.json();
+
+                if (res.ok) {
+                    setCurrentWeather(result);
+                } else {
+                    setErrorMessage(result.message);
+                }
+            } catch (error) {
+                setErrorMessage(error.message);
+            }
+        })();
     }, []);
 
-    const load = async () => {
-        try {
-            // const { status } = await Location.requestPermissionsAsync();
-            // if (status !== 'granted') {
-            //     setErrorMessage('Access to location is needed to run the app');
-            //     return;
-            // }
-            // const location = await Location.getCurrentPositionAsync();
-            // const { latitude, longitude } = location.coords;
-            // const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`;
-            const weatherUrl = `${BASE_WEATHER_URL}lat=-37.883642&lon=145.181741&units=${unitSystem}&appid=${WEATHER_API_KEY}`;
-
-            const res = await fetch(weatherUrl);
-            const result = await res.json();
-
-            if (res.ok) {
-                setCurrentWeather(result);
-            } else {
-                setErrorMessage(result.message);
-            }
-        } catch (error) {
-            setErrorMessage(error.message);
-        }
-    };
-
     if (currentWeather) {
-        const {
-            main: { temp },
-        } = currentWeather;
-
         return (
             <View style={styles.screen}>
                 <StatusBar style='auto' />
                 <View style={styles.main}>
-                    <Text>{temp}</Text>
+                    <WeatherInfo currentWeather={currentWeather} />
                 </View>
             </View>
         );
